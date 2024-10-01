6d27f926db9cfad14b26e1e70c7126456a6bc440
samuelloganbjss <samuel.logan@bjss.com>
2024-10-01 11:46:01 +0100

    - Added steel-thread between frontend and backend - front end page displays list of students - Added a test for the API getStudents method

--- backend/main.go
+++ backend/main.go
@@ -1,39 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
 	"io"
+	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func rootHandler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hello, welcome to the Feedback tool for the academy!")
+	fmt.Fprintf(writer, "Hello, welcome to the Feedback tool for the academy!")
 }
 
 func getTutors(writer http.ResponseWriter, request *http.Request) {
-	
-    fmt.Printf("got /api/tutors request\n")
+
+	fmt.Printf("got /api/tutors request\n")
 	io.WriteString(writer, "a list of all tutors from the db")
 
 }
 
 func getStudents(writer http.ResponseWriter, request *http.Request) {
-	
-    fmt.Printf("got /api/students  request\n")
+
+	fmt.Printf("got /api/students  request\n")
 	io.WriteString(writer, "a list of all students from the db")
 
 }
 
 func main() {
 
-    http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/api/students", getStudents)
 	http.HandleFunc("/api/tutors", getTutors)
 
-    fmt.Println("Server listening on port 8080...")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
-}
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
 
+	handler := c.Handler(http.DefaultServeMux)
+
+	fmt.Println("Server listening on port 8080...")
+	err := http.ListenAndServe(":8080", handler)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
